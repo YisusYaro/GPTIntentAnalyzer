f5035110b33d8365e347bc55a32daf1c3603165c
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-10-03 14:22:05 -0600

    test: create sentence

---
+++ test/sentences/interface/create-sentence.test.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import {
+  cleanUpMetadata,
+  InversifyExpressServer,
+} from 'inversify-express-utils';
+import request from 'supertest';
+import { App } from '../../../src/shared/infrastructure/dependency-injection/app';
+import { buildExpressApp } from '../../helpers/build-express-app';
+import '../../../src/sentences/interface/sentences.controller';
+
+describe('Create sentence', () => {
+  let app: any;
+  let container: Container;
+
+  beforeAll(async () => {
+    App.getInstance().setSharedModule();
+    App.getInstance().setSentencesModule();
+    container = App.getInstance().getContainer();
+    const server = new InversifyExpressServer(container);
+    app = buildExpressApp(server);
+  });
+
+  beforeEach(async () => {
+    container.snapshot();
+    cleanUpMetadata();
+  });
+
+  afterEach(() => container.restore());
+
+  describe('execute', () => {
+    it('should return successful http status', async () => {
+      const result = await request(app)
+        .post(`/sentences`)
+        .send({
+            id: '01HBVJJFMPT5GG7071ED3ANJFC',
+            text: 'Deseo tener más información de su producto',
+          });
+
+      expect(result.status).toBe(201);
+    }, 10000);
+  });
+});
