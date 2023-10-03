a2cc6e4e4d29e770a54f4b064249a0d1616991cc
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-10-03 13:52:46 -0600

    feat: log the query execution

--- src/shared/infrastructure/query-bus/query-bus.ts
+++ src/shared/infrastructure/query-bus/query-bus.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { Query } from '../../application/query';
 import { QueryHandlersInformation } from './query-handlers-information';
 import { TYPES } from '../dependency-injection/types';
+import { Logger } from '../../domain/logger';
 
 export interface QueryBus {
   execute(query: Query): Promise<any>;
@@ -12,10 +13,15 @@ export class QueryBusImpl implements QueryBus {
   constructor(
     @inject(TYPES.QueryHandlersInformation)
     private queryHandlersInformation: QueryHandlersInformation,
+    @inject(TYPES.Logger)
+    private logger: Logger,
   ) {}
 
   async execute(query: Query): Promise<any> {
+    this.logger.info(JSON.stringify(query));
     const handler = this.queryHandlersInformation.search(query);
-    return await handler.handle(query);
+    const result = await handler.handle(query);
+    this.logger.info(JSON.stringify(result));
+    return result;
   }
 }
